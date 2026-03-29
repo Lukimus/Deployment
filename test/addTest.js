e410ffa4c7e3633a67b924900907e84b02ce8014
Lukimus <lukalep05@gmail.com>
2026-03-29 18:38:09 +0300

    commit

--- test/addTest.js
+++ test/addTest.js
@@ -1,7 +1,12 @@
 
-const { expect } = require('chai');
-const { default: add } = require('../src/add.js');
+/*  const { expect } = import('chai');
+const { default: add } = import( {add} ,'../src/add.js');*/
 
+import {expect} from 'chai'
+
+
+
+import add from '../src/add.js'
 describe('test file', function() {
 
   beforeEach(function() {
